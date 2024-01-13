83ee7533991d72d4d8b5dc33e9bcc78725b60a16
Olivia Daniels <danielsolivia4199@gmail.com>
2024-01-13 13:23:25 -0600

    post orderItem working

--- hhpawapi/views/order_item.py
+++ hhpawapi/views/order_item.py
@@ -1,10 +1,12 @@
 """View module for handling requests about orders"""
 from django.http import HttpResponseServerError
+from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import ValidationError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from rest_framework.decorators import action
-from hhpawapi.models import OrderItem
+from hhpawapi.models import OrderItem, Order, Item
 
 class OrderItemView(ViewSet):
     """HHPAW order item view"""
@@ -39,6 +41,17 @@ class OrderItemView(ViewSet):
             return Response(serializer.data)
         else:
             return Response({'message': 'Order ID is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request):
+        try:
+            order = get_object_or_404(Order, pk=request.data["order"])
+            item = get_object_or_404(Item, pk=request.data["item"])          
+            order_item = OrderItem.objects.create(order=order, item=item)
+            serializer = OrderItemSerializer(order_item)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ValidationError as e:
+            return Response({'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
 class OrderItemSerializer(serializers.ModelSerializer):
     """JSON serializer for game types
     """
